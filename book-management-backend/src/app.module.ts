125ddd56720316df0c147a3acf93ab25ebe6601f
Ibtissemkr <ibtissem.kraiem2@gmail.com>
2025-02-08 20:58:19 +0100

    Build fix

--- book-management-backend/src/app.module.ts
+++ book-management-backend/src/app.module.ts
@@ -1,27 +1,26 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BooksModule } from './books/books.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { ConfigService } from '@nestjs/config';
-import { BookService } from './book/book.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true, 
-    }),
-    MongooseModule.forRootAsync
-    ({imports:[ConfigModule],
-      inject:[ConfigService],
-      useFactory: (configService: ConfigService) => ({
-      uri: configService.get<string>('MONGO_URI'), 
-    }),
-  }),
-    
-    BooksModule],
-  controllers: [AppController],
-  providers: [AppService, BookService],
+ imports: [
+ ConfigModule.forRoot({ isGlobal: true }),
+
+ MongooseModule.forRootAsync({
+ imports: [ConfigModule],
+ inject: [ConfigService],
+ useFactory: (configService: ConfigService) => ({
+ uri: configService.get<string>('MONGO_URI') ,
+ }),
+ }),
+
+ BooksModule, 
+ ],
+ controllers: [AppController],
+ providers: [AppService],
 })
 export class AppModule {}
+
--- book-management-backend/src/books/books.module.ts
+++ book-management-backend/src/books/books.module.ts
@@ -4,10 +4,12 @@ import { BooksService } from './books.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Book, BookSchema } from './book.schema';
 
-
 @Module({
-  imports:[MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }])],
-  controllers: [BooksController],
-  providers: [BooksService]
+ imports: [
+ MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]), 
+ ],
+ controllers: [BooksController],
+ providers: [BooksService],
+ exports: [BooksService],
 })
 export class BooksModule {}
